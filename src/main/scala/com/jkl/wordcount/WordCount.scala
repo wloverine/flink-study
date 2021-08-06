9289aa139c487f88a3aa0cb1d634798d1c3c1ed3
wloverine <1524161733@qq.com>
2021-08-06 14:11:11 +0800

    pass file name by ParameterTool

--- src/main/scala/com/jkl/wordcount/WordCount.scala
+++ src/main/scala/com/jkl/wordcount/WordCount.scala
@@ -1,13 +1,18 @@
 package com.jkl.wordcount
 
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.api.scala._
 
 object WordCount extends App {
   //创建执行环境
   val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
 
+  val tool: ParameterTool = ParameterTool.fromArgs(args)
+
+  val file: String = tool.get("file")
+
   //读取文本文件
-  env.readTextFile("/Users/daryl/IdeaProjects/flink-study/src/main/resources/test.txt")
+  env.readTextFile(file)
     .flatMap(_.split(" "))
     .map((_, 1))
     .groupBy(0)
