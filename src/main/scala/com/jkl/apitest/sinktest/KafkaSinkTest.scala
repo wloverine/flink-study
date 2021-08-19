2db9e55683abaafd8fec695674afdde7680f0f00
wloverine <1524161733@qq.com>
2021-08-19 21:38:55 +0800

    新增写入文件的方式

--- src/main/scala/com/jkl/apitest/sinktest/KafkaSinkTest.scala
+++ src/main/scala/com/jkl/apitest/sinktest/KafkaSinkTest.scala
@@ -1,7 +1,10 @@
 package com.jkl.apitest.sinktest
 
 import com.jkl.apitest.sourcetest.SensorReading
+import org.apache.flink.api.common.serialization.SimpleStringEncoder
 import org.apache.flink.api.scala._
+import org.apache.flink.core.fs.Path
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011
 import org.apache.flink.streaming.util.serialization
@@ -22,8 +25,14 @@ object KafkaSinkTest {
     //写入socket流
 //    dataStream.writeToSocket("localhost", 9999, new SimpleStringSchema())
 
+    //写入到文件
+    dataStream.addSink(StreamingFileSink.forRowFormat(
+      new Path("/Users/daryl/IdeaProjects/flink-study/src/main/resources/res.txt"),
+      new SimpleStringEncoder[String]("utf-8")
+    ).build())
+
     //写入kafka
-    dataStream.addSink(new FlinkKafkaProducer011[String]("localhost:9092","sensor",new SimpleStringSchema()))
+//    dataStream.addSink(new FlinkKafkaProducer011[String]("localhost:9092","sensor",new SimpleStringSchema()))
 
     env.execute("KafkaSinkTest")
   }
