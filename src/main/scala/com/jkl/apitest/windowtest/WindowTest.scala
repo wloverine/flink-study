5afb8981eda5468461429d89c908e3aa6c8a044c
wloverine <1524161733@qq.com>
2021-10-21 17:27:53 +0800

    添加对window偏移量的解释

--- src/main/scala/com/jkl/apitest/windowtest/WindowTest.scala
+++ src/main/scala/com/jkl/apitest/windowtest/WindowTest.scala
@@ -1,25 +1,29 @@
 package com.jkl.apitest.windowtest
 
 import com.jkl.apitest.sourcetest.MySensorSource
-import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
 
 
 /**
- * 每间隔10s，统计一次最高温度
+ * 每间隔一定时间，统计一次最高温度
  */
 object WindowTest {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     
-    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
-
     env.addSource(new MySensorSource())
       .keyBy(_.id)
-//      .timeWindow(Time.minutes(1),Time.seconds(10))
-      .window(TumblingEventTimeWindows.of(Time.days(1)))
+      //      .timeWindow(Time.minutes(1),Time.seconds(10))
+      .window(
+        TumblingProcessingTimeWindows.of(
+          //第一个参数是每多少时间统计一次。第二个参数是偏移量。
+          //此处就代表每分钟的第20s会计算一次窗口。注意如果想要每天统计一次的话，需要这么写:
+          //Time.days(1),Time.hours(-8)。因为中国时区是utc+8，而此函数用的是格林威治时间，需要往前挪8小时。
+          Time.minutes(1),Time.seconds(20)
+        )
+      )
       .maxBy(2)
       .print()
 
